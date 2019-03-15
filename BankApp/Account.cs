d49d80c050efd3259db8e1394962415b6750274d
Renu Thottan <renuthottan@gmail.com>
2019-03-15 10:25:56 -0700

    Initial commit

---
+++ BankApp/Account.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BankApp
+{
+    class Account
+    {
+        public int AccountNumber { get; set; }
+        public string EmailAddress { get; set; }
+        public decimal Balance { get; set; }
+        public string AccountType { get; set; }
+        public DateTime CreatedDate { get; set; }
+
+    }
+}
